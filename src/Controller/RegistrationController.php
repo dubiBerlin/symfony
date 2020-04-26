67909a45b0bf8c9e9f4811992e7acc233b6158b6
DubiBerlin <woose@web.de>
2020-04-26 17:07:20 +0200

    add SubmitType to form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RegistrationController extends AbstractController
 {
@@ -16,13 +17,19 @@ class RegistrationController extends AbstractController
     {
 
       $form = $this->createFormBuilder()
-      ->add("username")->add("password", RepeatedType::class, [
+      ->add("username")
+      ->add("password", RepeatedType::class, [
         'type' => PasswordType::class,
         'invalid_message' => 'The password fields must match.',
         'options' => ['attr' => ['class' => 'password-field']],
         'required' => true,
         'first_options'  => ['label' => 'Password'],
         'second_options' => ['label' => 'Confirm Password'],
+      ])
+      ->add("Registrieren",SubmitType::class,[
+        "attr"=>[
+          "class"=>"btn btn-success float-right"
+        ]
       ])
       ->getForm();
 
