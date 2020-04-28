a984a429c6bf7631f9a079240f98818b99e007c0
DubiBerlin <woose@web.de>
2020-04-28 15:36:25 +0200

    add category to formbuilder

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EntityType;
 
 class PostType extends AbstractType
 {
@@ -15,6 +17,9 @@ class PostType extends AbstractType
     {
         $builder
             ->add('title')
+            ->add('category',EntityType::class,[
+              "class"=>Category::class
+            ])
             ->add('attachment',FileType::class,[
               "mapped"=>false
             ])
