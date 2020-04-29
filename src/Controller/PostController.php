022be75fe62502e28fb5994dc31e580a8dd01493
DubiBerlin <woose@web.de>
2020-04-29 08:27:00 +0200

    create findAllPostByUser($user) in PostRepository

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -26,13 +26,9 @@ class PostController extends AbstractController
      */
     public function index(PostRepository $postRepository)
     {
-      
-      
       $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-      $user = $this->getUser();
-      dump($user->getId());
       
-      $posts = $postRepository->findAll();
+      $posts = $postRepository->findAllPostByUser($this->getUser());
 
       return $this->render('post/index.html.twig', [
         'posts' => $posts,
