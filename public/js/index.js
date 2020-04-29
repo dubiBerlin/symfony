f1fdc48229263f7039c558742ed50115f5957229
DubiBerlin <woose@web.de>
2020-04-29 15:32:33 +0200

    pass route to vue

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,41 @@
 const vm = new Vue({
   el: '#app',
   name: "postLists", 
-  props: {
-    posts: {
-      type: Array,
-      required: true
-    }
-  },
   template: `
   <div id="app">
   <div>
     <button type="button" @click="add">Add</button>
     <button type="button" @click="remove">Remove</button>
-    <button type="button" @click="shuffle">Shuffle</button>
     <button type="button" @click="reverse">Sortieren</button>
   </div>
   <transition-group tag="ul" name="list">
     <li v-for="item in list" :key="item.id">
-      Item {{ item.id }}
+      id: {{ item.id }}<br/>
+      title:  {{ item.title }}
+      <!--<button class="btn btn-danger" @click="deletePost(item.id)" >Delete</button>-->
+      <a class="btn btn-danger" :href="pathDelete.replace('placeholderId',item.id)">Delete</a>
     </li>
   </transition-group>
 </div>
   `,
   data: {
+    pathShow: "",
+    pathDelete: "",
+    posts:"",
     maxId: 3,
-    list: [],
-    db: [
-      { id: 1 },
-      { id: 2 },
-      { id: 3 },
-      { id: 4 },
-      { id: 5 }
-    ]
+    list: []
+  },
+  beforeMount: function () {
+    this.posts = JSON.parse(this.$el.attributes['data-posts'].value);
+    this.pathShow = this.$el.attributes['data-pathShow'].value;
+    this.pathDelete = this.$el.attributes['data-pathDelete'].value;
+    console.log("BEFORE MOUNT: ",typeof this.pathShow,"\n",this.pathDelete);
   },
   mounted() {
+    // console.log("MOUNTED");
     this.t = setInterval(() => {
       this.intervallFunction();
-    }, 1000);
+    }, 500);
     console.log(this.posts);
   },
   methods: {
@@ -47,12 +46,14 @@ const vm = new Vue({
       return Math.floor(Math.random() * (max + 1))
     },
     intervallFunction() {
-      console.log("hello")
-      if (this.db.length > 0) {
-        this.list.push(this.db.pop());
+      if (this.posts.length > 0) {
+        this.list.push(this.posts.pop());
       } else {
         window.clearInterval(this.t);
       }
+    },
+    deletePost(id,event){
+      Window.open(this.pathDelete.replace("placeholderId",id));
     },
     add() {
       const id = ++this.maxId
@@ -62,15 +63,6 @@ const vm = new Vue({
     remove() {
       const index = this.random(this.list.length - 1)
       this.list.splice(index, 1)
-    },
-    shuffle() {
-      const shuffled = []
-      while (this.list.length > 0) {
-        const index = this.random(this.list.length - 1)
-        shuffled.push(this.list[index])
-        this.list.splice(index, 1)
-      }
-      this.list = shuffled
     }
   }
 });
